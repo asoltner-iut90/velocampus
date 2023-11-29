696ff3b3086df8c723eeed0419471690dbb846df
asoltner-iut90 <146073354+asoltner-iut90@users.noreply.github.com>
2023-11-29 11:55:01 +0100

    DropDown

--- static/mon_script.js
+++ static/mon_script.js
@@ -1,19 +1,6 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function redirectToPage(select) {
+  var selectedPage = select.value;
+  if (selectedPage) {
+    window.location = selectedPage; // Redirection vers la page sélectionnée
   }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+}
